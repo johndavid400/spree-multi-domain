bbd6aca0fd2ed7d8b3e579320f33bfdeeae4b036
johndavid400 <jd@isotope11.com>
2012-08-15 10:23:20 -0500

    adding attr accessible for store_id to fix mass assignment security error when assigning a store to a product on the products edit page

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Order.class_eval do
   belongs_to :store
+  attr_accessible :store_id
   scope :by_store, lambda { |store| where(:store_id => store.id) }
 end
--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Tracker.class_eval do
   belongs_to :store
+  attr_accessible :store_id, :store
 
   def self.current(domain)
     Spree::Tracker.where(:active => true, :environment => ENV['RAILS_ENV']).joins(:store).where("spree_stores.domains LIKE '%#{domain}%'").first
